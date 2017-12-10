aef4200d7349d702ebc548f211d3a73563d94488
Tine <tine@equaleyes.com>
2017-12-10 01:28:12 +0000

    Test: move mocked response Json to separate file

--- bittrex/test/bittrex_tests.py
+++ bittrex/test/bittrex_tests.py
@@ -32,37 +32,20 @@ def mocked_cancel_query(protection=None, path_dict=None, options=None):
 
 
 def mocked_get_order_query(protection=None, path_dict=None, options=None):
-    return json.loads(
-                        '{\
-                            "success" : "true", \
-                            "message" : "", \
-                            "result" : { \
-                                "AccountId" : null, \
-                                "OrderUuid" : "0cb4c4e4-bdc7-4e13-8c13-430e587d2cc1", \
-                                "Exchange" : "BTC-SHLD", \
-                                "Type" : "LIMIT_BUY", \
-                                "Quantity" : 1000.00000000, \
-                                "QuantityRemaining" : 1000.00000000, \
-                                "Limit" : 0.00000001, \
-                                "Reserved" : 0.00001000, \
-                                "ReserveRemaining" : 0.00001000, \
-                                "CommissionReserved" : 0.00000002, \
-                                "CommissionReserveRemaining" : 0.00000002,\
-                                "CommissionPaid" : 0.00000000, \
-                                "Price" : 0.00000000, \
-                                "PricePerUnit" : null, \
-                                "Opened" : "2014-07-13T07:45:46.27", \
-                                "Closed" : null, \
-                                "IsOpen" : true, \
-                                "Sentinel" : "6c454604-22e2-4fb4-892e-179eede20972", \
-                                "CancelInitiated" : false, \
-                                "ImmediateOrCancel" : false, \
-                                "IsConditional" : false, \
-                                "Condition" : "NONE", \
-                                "ConditionTarget" : null \
-                            } \
-                        }'
-                    )
+    """
+    Mock Bittrex API response for 'get order' query
+
+    Endpoint:
+    1.1 /account/getorder
+    2.0 /key/orders/getorder
+
+    Docs & response example:
+    https://bittrex.com/Home/Api
+    """
+    with open(os.path.abspath("bittrex/test/getorder-response.json")) as order_response:
+        json_response = json.load(order_response)
+        order_response.close()
+        return json_response
 
 
 class TestBittrexV11PublicAPI(unittest.TestCase):
